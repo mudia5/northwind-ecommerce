3c7f44316076ae6050f8ea001f52de9c632661fe
AndrewKallmeyer1 <90013113+AndrewKallmeyer1@users.noreply.github.com>
2025-02-20 00:33:24 -0500

    Database Initalization

--- app/schema.sql
+++ app/schema.sql
@@ -1,5 +1,7 @@
 -- DROP TABLE IF EXISTS user;
 -- DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS Authentication;
+DROP TABLE IF EXISTS Shopping_Cart;
 
 CREATE TABLE Authentication (
   userID TEXT PRIMARY KEY,
@@ -10,12 +12,10 @@ CREATE TABLE Authentication (
 CREATE TABLE Shopping_Cart (
   shopperID TEXT NOT NULL,
   productID INTEGER NOT NULL,
-  QUANTITY INTEGER NOT NULL CHECK (quantity > 0),
+  QUANTITY INTEGER NOT NULL CHECK (QUANTITY > 0),
   title TEXT NOT NULL,
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO Employees (
-    (LastName, FirstName)
-    VALUES ('WEB', 'WEB')
-);
+INSERT INTO Employees (LastName, FirstName)
+VALUES ('WEB', 'WEB');

227beea76e522312b73d2455402fd3e97983a5cb
meichelman <meichelman5@gmail.com>
2025-04-30 11:34:49 -0400

    Fix bugs

--- P3/app/groups.py
+++ P3/app/groups.py
@@ -76,7 +76,7 @@ def create() -> Union[str, Response, Tuple[Response, int], Tuple[str, int]]:
         website = request.form.get('website', '').strip()
         min_age = request.form.get('min_age', '')
         max_age = request.form.get('max_age', '')
-        sign_up_price = request.form.get('sign_up_price', type)
+        sign_up_price = request.form.get('sign_up_price')
         if not website:
             website = ''
         if not min_age:
@@ -87,29 +87,32 @@ def create() -> Union[str, Response, Tuple[Response, int], Tuple[str, int]]:
             sign_up_price = '0'
         curr_time = datetime.now(timezone.utc).strftime(r'%Y-%m-%d')
         db: sqlite3.Connection = get_db()
-        db.execute(
-            """
-            INSERT INTO Groups (group_name, group_description, contact_email, website_url, min_age,
-            max_age, sign_up_price, register_date)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            """,
-            (group_name, description, email, website, min_age, max_age, sign_up_price,
-             curr_time)
-        )
-        db.execute(
-            """
-            INSERT INTO Belongs (group_name, category_name,)
-            VALUES (?, ?)
-            """,
-            (group_name, category)
-        )
-        db.execute(
-            """
-            INSERT INTO Membership (group_name, user_id, user_role, join_time, register_time)
-            VALUES (?, ?, ?, ?, ?)
-            """,
-            (group_name, g.user['user_id'], 'admin', curr_time, curr_time)
-        )
-        db.commit()
-        return redirect(url_for('browse.groups'))
+        try:
+            db.execute(
+                """
+                INSERT INTO Groups (group_name, group_description, contact_email, website_url,
+                min_age, max_age, sign_up_price, register_date)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                """,
+                (group_name, description, email, website, min_age, max_age, sign_up_price,
+                curr_time)
+            )
+            db.execute(
+                """
+                INSERT INTO Belongs (group_name, category_name)
+                VALUES (?, ?)
+                """,
+                (group_name, category)
+            )
+            db.execute(
+                """
+                INSERT INTO Membership (group_name, user_id, user_role, join_time, register_time)
+                VALUES (?, ?, ?, ?, ?)
+                """,
+                (group_name, g.user['user_id'], 'admin', curr_time, curr_time)
+            )
+            db.commit()
+        except sqlite3.IntegrityError:
+            flash('User does not meet the age requirement for this group.')
+        return redirect(url_for('browse.groups', category='all'))
     return render_template('groups/create.html')
